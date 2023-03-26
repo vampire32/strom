a37c9d642bacb154dc214e6dadba6ed016b5d7e9
vampire32 <83330664+vampire32@users.noreply.github.com>
2023-03-26 21:56:44 +0500

    d

--- pages/Home2.js
+++ pages/Home2.js
@@ -835,7 +835,7 @@ export default function Home2() {
 											border: "5px solid black",
 											borderRadius: "15px",
 											borderColor: "rgba(0,0,0,0.3)",
-											height:"300px",
+											height:"270px",
 										}}
 										src={S1}
 										alt="Car Tinting Service Sharjah"
